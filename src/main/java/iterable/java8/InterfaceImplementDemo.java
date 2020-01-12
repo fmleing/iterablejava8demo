2cfcaaf6acd6da2cb710394819984fb59a5a0aa7
Mengleifeng <fmleing@163.com>
2020-01-12 20:11:00 +0800

    实现类编写

---
+++ src/main/java/iterable/java8/InterfaceImplementDemo.java
@@ -0,0 +1,22 @@
+package iterable.java8;
+
+public class InterfaceImplementDemo implements Interface1,Interface2 {
+    @Override
+    public void active(String str) {
+
+    }
+
+    /**
+     * 必须实现 log 这个方法
+     * @param str
+     */
+    @Override
+    public void log(String str) {
+
+    }
+
+    @Override
+    public void doSomething() {
+
+    }
+}
